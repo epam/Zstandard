031452c365dc798427fcd3928803e81163632417
Andrei Davydov <adavydov@deltixlab.com>
2021-12-10 15:39:00 +0300

    test: Add tests for put* and get* operations.

--- java/src/test/java/com/epam/deltix/zstd/TestDecompress.java
+++ java/src/test/java/com/epam/deltix/zstd/TestDecompress.java
@@ -1,6 +1,10 @@
 package com.epam.deltix.zstd;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Random;
 
 public class TestDecompress {
     @Test
@@ -17,5 +21,33 @@ public class TestDecompress {
         decompressor.decompress(
                 compressedData, 0, compressedData.length,
                 decompressedData, 0, decompressedData.length);
+    }
+
+    @Test
+    public void testPutGet() {
+        final byte[] input = new byte[1000000];
+        final byte[] output = new byte[input.length];
+
+        new Random().nextBytes(input);
+
+        Arrays.fill(output, (byte) -1);
+        for (int i = 0; i < input.length; i += 1)
+            Util.putByte(output, i, Util.getByte(input, i));
+        Assert.assertArrayEquals(input, output);
+
+        Arrays.fill(output, (byte) -1);
+        for (int i = 0; i < input.length; i += 2)
+            Util.putShort(output, i, Util.getShort(input, i));
+        Assert.assertArrayEquals(input, output);
+
+        Arrays.fill(output, (byte) -1);
+        for (int i = 0; i < input.length; i += 4)
+            Util.putInt(output, i, Util.getInt(input, i));
+        Assert.assertArrayEquals(input, output);
+
+        Arrays.fill(output, (byte) -1);
+        for (int i = 0; i < input.length; i += 8)
+            Util.putLong(output, i, Util.getLong(input, i));
+        Assert.assertArrayEquals(input, output);
     }
 }
